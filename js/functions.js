95a0dee5507fc756f00eac6a83c0799c24d23e8f
David Reinberger <davidreinberger@gmail.com>
2014-02-24 22:04:07 +0100

    Added beatport search function
    proof of concept

--- js/functions.js
+++ js/functions.js
@@ -47,9 +47,31 @@ function CreateTrackTable (data, length) {
                     $("#track-table").append('<tr path="' + FilePath + '" metas="' + result.artist + '|' + result.title + '|' + result.album + '|' + result.genre + '|' + 'data: image/' + result.picture[0].format + '; base64, ' + picture + '"><td><div class="table-artwork"><img src="data: image/' + result.picture[0].format + '; base64, ' + picture +'"/></div><div class="table-track-info"><strong>' + result.title + '</strong><br>' + result.artist + '</div></td></tr>');     
                 }
             });
-            
         }
     }
+}
+
+function searchBeatport (artist, title) {
+    
+    var sys   = require('sys');
+    var OAuth = require('oauth').OAuth;
+    var oa    = new OAuth("https://oauth-api.beatport.com/catalog/3/search/",
+                      "https://oauth-api.beatport.com/catalog/3/search/",
+                      beatportApiKey, beatportApiSecret,
+                      "1.0A", undefined, "HMAC-SHA1");
+    
+        
+    var query = 'query=' + title + '&facets=artistName:' + artist,
+        url   = 'https://oauth-api.beatport.com/catalog/3/search/?' + query;
+    
+    var request = oa.get(url, beatportAccessToken, beatportTokenSecret, function(error, data) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(JSON.parse(data));
+        }
+    });
+
 }
 
 
@@ -59,53 +81,53 @@ function CreateTrackTable (data, length) {
 // http://jsperf.com/encoding-xhr-image-data/5
  
 function base64ArrayBuffer(arrayBuffer) {
-  var base64    = ''
-  var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+  var base64    = '';
+  var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
  
-  var bytes         = new Uint8Array(arrayBuffer)
-  var byteLength    = bytes.byteLength
-  var byteRemainder = byteLength % 3
-  var mainLength    = byteLength - byteRemainder
+  var bytes         = new Uint8Array(arrayBuffer);
+  var byteLength    = bytes.byteLength;
+  var byteRemainder = byteLength % 3;
+  var mainLength    = byteLength - byteRemainder;
  
-  var a, b, c, d
-  var chunk
+  var a, b, c, d;
+  var chunk;
  
   // Main loop deals with bytes in chunks of 3
   for (var i = 0; i < mainLength; i = i + 3) {
     // Combine the three bytes into a single integer
-    chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+    chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
  
     // Use bitmasks to extract 6-bit segments from the triplet
-    a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
-    b = (chunk & 258048)   >> 12 // 258048   = (2^6 - 1) << 12
-    c = (chunk & 4032)     >>  6 // 4032     = (2^6 - 1) << 6
-    d = chunk & 63               // 63       = 2^6 - 1
+    a = (chunk & 16515072) >> 18 ;// 16515072 = (2^6 - 1) << 18
+    b = (chunk & 258048)   >> 12 ;// 258048   = (2^6 - 1) << 12
+    c = (chunk & 4032)     >>  6 ;// 4032     = (2^6 - 1) << 6
+    d = chunk & 63 ;              // 63       = 2^6 - 1
  
     // Convert the raw binary segments to the appropriate ASCII encoding
-    base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+    base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
   }
  
   // Deal with the remaining bytes and padding
   if (byteRemainder == 1) {
-    chunk = bytes[mainLength]
+    chunk = bytes[mainLength];
  
-    a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+    a = (chunk & 252) >> 2 ;// 252 = (2^6 - 1) << 2
  
     // Set the 4 least significant bits to zero
-    b = (chunk & 3)   << 4 // 3   = 2^2 - 1
+    b = (chunk & 3)   << 4 ;// 3   = 2^2 - 1
  
-    base64 += encodings[a] + encodings[b] + '=='
+    base64 += encodings[a] + encodings[b] + '==';
   } else if (byteRemainder == 2) {
-    chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+    chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
  
-    a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
-    b = (chunk & 1008)  >>  4 // 1008  = (2^6 - 1) << 4
+    a = (chunk & 64512) >> 10 ;// 64512 = (2^6 - 1) << 10
+    b = (chunk & 1008)  >>  4 ;// 1008  = (2^6 - 1) << 4
  
     // Set the 2 least significant bits to zero
-    c = (chunk & 15)    <<  2 // 15    = 2^4 - 1
+    c = (chunk & 15)    <<  2 ;// 15    = 2^4 - 1
  
-    base64 += encodings[a] + encodings[b] + encodings[c] + '='
+    base64 += encodings[a] + encodings[b] + encodings[c] + '=';
   }
   
-  return base64
+  return base64;
 }
--- js/static.js
+++ js/static.js
@@ -20,9 +20,11 @@ $('#track-table').on('click', 'tr', function() {
         rawMeta = $(this).attr("metas"),
         meta = rawMeta.split("|");
     
-        $( "#info-track-title" ).html(meta[0]);
-        $( "#info-track-artist" ).html(meta[1]);
-        $( "#info-track-album" ).html(meta[2]);
-        $( "#info-track-genre" ).html(meta[3]);
-        $( "#central-img" ).attr("src",meta[4]);
+        $( "#info-track-title" ).html(meta[1]); //track name
+        $( "#info-track-artist" ).html(meta[0]); //track artist
+        $( "#info-track-album" ).html(meta[2]); //album name
+        $( "#info-track-genre" ).html(meta[3]); //track genre
+        $( "#central-img" ).attr("src",meta[4]); //track cover
+
+    searchBeatport(meta[0], meta[1]);
 });
